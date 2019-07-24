8c0c6742393f5c7917ceae8b4bc740034c093a2c
Yutian Han <kevinhangoat@gmail.com>
2019-07-24 05:31:56 -0400

    finak

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  restaurant = Restaurant.new(
+                  name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+                  phone_number: Faker::PhoneNumber.cell_phone)
+  10.times do
+    restaurant.reviews.new(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5)
+    )
+  end
+  restaurant.save
+end
